701d9fefbdc6af59818b15e2c2dd7a8b7e0dabbc
bluewhale <lqtu12369@gmail.com>
2024-11-29 16:40:36 +0700

    fix: not show metadata

--- src/app/(standalone)/workspaces/[workspaceId]/join/[inviteCode]/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/join/[inviteCode]/page.tsx
@@ -1,31 +1,4 @@
 import { InviteWorkspacePage } from "@/features/workspace/components/invite-workspace";
-import { getWorkspace } from "@/features/workspace/queries/get-workspace";
-import { Metadata, ResolvingMetadata } from "next";
-
-type Props = {
-    params: {
-        workspaceId: string;
-    };
-};
-
-export async function generateMetadata(
-    { params }: Props,
-    parent: ResolvingMetadata,
-): Promise<Metadata> {
-    const { workspaceId } = params;
-
-    const workspace = await getWorkspace(workspaceId);
-
-    const previousImages = (await parent).openGraph?.images || [];
-
-    return {
-        title: workspace.name,
-        description: `Join ${workspace.name} to collaborate with your team and get things done together.`,
-        openGraph: {
-            images: [...previousImages],
-        },
-    };
-}
 
 export default function Page() {
     return <InviteWorkspacePage />;
